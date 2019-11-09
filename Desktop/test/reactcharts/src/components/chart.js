5342fd80899453071953a966de972566c396857c
arminensi <arminensi@hotmail.com>
2019-11-09 23:57:32 +0100

    first commit1

---
+++ Desktop/test/reactcharts/src/components/chart.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'React';
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
+class Chart extends Component {
+	render(){
+		return (
+			<div Classname= 'chart'>
+				CHART COMPONENT
+					</div>
+				)
+		}
+	}
+
+export default Chart;
